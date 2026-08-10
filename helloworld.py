e16d3dcbdf7a5408dc1e4cad2fa84596a3662751
Vic Abalos <vabalos@m360.com.ph>
2026-08-10 09:36:06 +0800

    asd

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+# this is a comments
+""" this is multi line
+vic abalos from m360
+doing for a living Software Engineer
+matra Small improvements. Every day.
+interest - i like playing gitar, tinkering stuff
+reason - to have fundamentals on python
+"""
+
+print("hello world")
