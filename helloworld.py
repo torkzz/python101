098d2ce011e33376a183e78775c6325940c3c5a5
Vic Abalos <vabalos@m360.com.ph>
2026-08-10 10:02:34 +0800

    float

--- helloworld.py
+++ helloworld.py
@@ -14,10 +14,48 @@ reason - to have fundamentals on python
 # print(199.9312)
 
 
-print(10+2)
-print(10-2)
-print(10*2)
-print(10/2)
-print(2/10)
-print(20/4)
-print(20//4)
+# print(10+2)
+# print(10-2)
+# print(10*2)
+# print(10/2)
+# print(2/10)
+# print(20/4)
+# print(20//4)
+
+# X = "Anja"
+# Y = 95
+# Z = True
+
+# print (X)
+# print (Y)
+# print (Z)
+
+
+# test_var = 10
+# test_var_2 = 20
+# name = "John"
+# ID_number = "IT010101"
+
+# print(test_var)
+# print(test_var_2)
+# print(name)
+# print(ID_number)
+
+
+
+# print(type (test_var))
+# print(type (test_var_2))
+# print(type (name))
+# print(type (ID_number))
+
+
+
+# Float
+nearly_pi = 3.1415
+
+print(nearly_pi)
+print(type (nearly_pi))
+
+almost_py = 22/7
+print(almost_py)
+print(type (almost_py))
