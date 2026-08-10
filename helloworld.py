7bbb33d270dd42317894a1d87edf2de0b7c90613
Vic Abalos <vabalos@m360.com.ph>
2026-08-10 11:34:07 +0800

    calculator

--- helloworld.py
+++ helloworld.py
@@ -51,27 +51,113 @@ reason - to have fundamentals on python
 
 
 # Float
-nearly_pi = 3.1415
+# nearly_pi = 3.1415
 
-print(nearly_pi)
-print(type (nearly_pi))
+# print(nearly_pi)
+# print(type (nearly_pi))
 
-almost_py = 22/7
-print(almost_py)
-print(type (almost_py))
+# almost_py = 22/7
+# print(almost_py)
+# print(type (almost_py))
+
+# print()
+# print()
+
+# almost_py = 22//7
+# rouded_pi = round(almost_py, 5)
+# print(almost_py)
+# print(type (almost_py))
+# print()
+# print()
+# print(rouded_pi)
+# print(type (rouded_pi))
+
+# rouded_pi = round(almost_py, 2)
+# print(rouded_pi)
+# print(type (rouded_pi))
+
+
+
+# Strings
+# my_number = "1.12321"
+# print(my_number)
+# print(type (my_number))
+# print(len (my_number))
+# print()
+# print()
+
+# also_my_number = float(my_number)
+# print(also_my_number)
+# print(type (also_my_number))
+
+
+# new_string = "abc" +" "+ "def"
+# print(new_string)
+# print(type (new_string))
+# print(len (new_string))
+
+
+# new_string = "Martha "+ "Flor"
+# print(new_string)
+# print(type (new_string))
+# print(len (new_string))
+
+
+# new_string = "====####====" *5
+# print(new_string)
+# print(type(new_string))
+
+
+# user_name = input("enter username")
+# user_color= input("enter color")
+# print(user_name)
+# print(type(user_name))
+
+# print(f" name  {user_name} color {user_color}")
+
+
+# age = int(input("what is you age"))
+# height = float(input("what is your height in cm"))
+
+
+# print(f"your age is {age} and you height is {height}")
+
+
+# a = int(input("ENter an integer:"))
+# b = int(input("ENter another int :"))
+
+# print ("Output:", a + b)
+
+
+# help(print)
+# help(len)
+
+
+# student = ("kevin", 20, "BSIT")
+# print(student)
+# print(type(student))
+
+# student = ["kevin", 20, "BSIT"]
+# print(student)
+# print(type(student))
+
+# student = {"kevin", 20, "BSIT"}
+# print(student)
+# print(type(student))
+
+
+# Module 1 Activity: Simple Mini Calculator
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
 
 print()
-print()
 
-almost_py = 22//7
-rouded_pi = round(almost_py, 5)
-print(almost_py)
-print(type (almost_py))
-print()
-print()
-print(rouded_pi)
-print(type (rouded_pi))
+print(f"sum = {sum_result}")
+print(f"diff = {difference_result}")
+print(f"mult = {product_result}")
 
-rouded_pi = round(almost_py, 2)
-print(rouded_pi)
-print(type (rouded_pi))
